8d862f3e93e5eddeae48cfd7e0da3a0ea16038de
Maria <monastyrskaya@Marias-MacBook-Air.local>
2025-11-21 21:43:07 +0000

    Fix missing Pressable import causing white screen

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, View, Text, StyleSheet, Platform } from 'react-native';
+import { ActivityIndicator, View, Text, Pressable, StyleSheet, Platform } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
