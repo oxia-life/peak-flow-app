bfbf7f2aa1085370eb860101543cbe81b0cb0599
Maria <monastyrskaya@Marias-MacBook-Air.local>
2025-12-07 11:05:54 +0000

    Bump versionCode to 10

--- app.config.js
+++ app.config.js
@@ -40,7 +40,7 @@ export default {
         backgroundColor: "#1E4C60"
       },
       package: "com.oxia.peakflowdiary",
-      versionCode: 9,
+      versionCode: 10,
       permissions: []
     },
     
